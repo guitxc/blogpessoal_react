11cdd76bc760934bc79b784a7b83f5630cc2a1a8
Guilherme <guisandolidna@gmail.com>
2026-08-18 11:42:38 -0300

    feat: integrate Tailwind CSS and add routing
    - Added Tailwind CSS for styling and updated index.css to import it.
    - Implemented React Router for navigation between Home, Cadastro, and Login pages.
    - Created Navbar component for site navigation.
    - Added Footer component with social media links.
    - Refactored Home component to use Tailwind CSS classes for layout and styling.
    - Created Cadastro and Login components as placeholders.
    - Updated package.json to include necessary dependencies for Tailwind CSS and React Router.

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+ 
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+                            bg-indigo-900 text-white'>
+           
+                <div className="container flex justify-between text-lg mx-8">
+                    <Link to="/" className="text-2xl font-bold">Blog Pessoal </Link>
+ 
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                       <Link to="/login" className="hover:underline">Sair </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+ 
+export default Navbar
